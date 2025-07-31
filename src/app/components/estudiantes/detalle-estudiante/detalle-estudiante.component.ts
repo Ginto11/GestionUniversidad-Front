9d4e3ae1a48e13e65a0cf26d0a10461552effc71
Ginto11 <salinitosnelson@gmail.com>
2025-07-30 22:33:01 -0500

    Corrigiendo redirecciones por medio de modales.

--- src/app/components/estudiantes/detalle-estudiante/detalle-estudiante.component.ts
+++ src/app/components/estudiantes/detalle-estudiante/detalle-estudiante.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Estudiante } from 'src/app/models/estudiante.model';
 import { FormsModule } from '@angular/forms';
 import { EstudianteServices } from 'src/app/services/estudiantes/estudiante.service';
 import { CommonModule } from '@angular/common';
+import { ModalService } from 'src/app/services/modal/modal.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Component({
     selector: 'app-detalle-estudiante',
@@ -11,17 +14,44 @@ import { CommonModule } from '@angular/common';
     styleUrl: './detalle-estudiante.component.css'
 })
 
-export default class DetalleEstudianteComponent {
+export default class DetalleEstudianteComponent implements OnInit {
     estudiante: Estudiante = new Estudiante();
 
     cedula = null;
 
-    constructor(private estudiantesService: EstudianteServices){}
+    constructor(
+        private authService: AuthService,
+        private modalService: ModalService,
+        private estudiantesService: EstudianteServices){}
+
+    ngOnInit(): void {
+        this.validarToken();
+    }
+
+    validarToken = async () :Promise<any> => {
+        if(await this.authService.validarSesion() == false){
+            this.modalService.abrirModal(ModalComponent, {
+                mensaje: 'Token expirado, inicie sesión nuevamente.',
+                altImg: 'Imagen de informacion',
+                colorTexto: '#1A1731',
+                srcImg: 'informacion.webp',
+                listaErrores: [],
+                redireccionar: true
+            })
+            return;
+        }
+    }
 
     buscarEstudiante = async () => {
-        console.log(this.estudiante)
         if(!this.cedula){
-            alert('Ingrese un numero de cedula.');
+            this.modalService.abrirModal(ModalComponent, {
+                mensaje: 'Ingrese el numero de cedula, por favor.',
+                altImg: 'Imagen de error',
+                colorTexto: 'Red',
+                srcImg: 'error.webp',
+                listaErrores: [],
+                redireccionar: false
+            })
             return;
         }
 
